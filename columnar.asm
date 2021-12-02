896d26a879159281a7e7576b3e25b168ff1aa673
Mihailescu Eduard <mihailescu.eduard@gmail.com>
2021-12-02 21:25:19 +0200

    Finished task 3

--- columnar.asm
+++ columnar.asm
@@ -1,6 +1,7 @@
 %include "io.mac.asm"
 section .data
     extern len_cheie, len_haystack
+    index dd 0
 
 section .text
     global columnar_transposition
@@ -28,6 +29,9 @@ columnar_transposition:
 
     ;; ecx iterate through key vector [0, len_cheie -1]
     ;; eax iterate through the column at index ecx
+    mov eax, [index]
+    xor eax, eax
+    mov [index], eax
 
     xor edx, edx
 
@@ -40,21 +44,65 @@ columnar_transposition:
 
 
     xor ecx, ecx
-    xor si, si
 
 columnar_loop:
 
     xor eax, eax
     ;; index of the current column
-    mov edx, [edi + (ecx * 4)]
+    
 
 column_loop:
+    mov edx, [edi + (ecx * 4)]
+
+    ;; get the index of the char in plain text
     push edi
     mov edi, [len_cheie]
     imul edi, eax
-    mov [ebx + si], [esi + edi]
+    
+    ;; verify 
+
+    ;; save the current index stored in eax
+    ;; and put the index of the current character of cypertext
+    push eax
+    mov eax, [index]
+
+    lea eax, [ebx + eax]
+    push ebx
+    mov ebx, esi
+    lea ebx, [ebx + edi]
+    lea ebx, [ebx + edx] 
+
+    mov bl, byte[ebx]
+    mov byte[eax], bl
+
+    cmp bl, 31
+    jle problem
+
+    jmp no_problem
+
+    cmp bl, 127
+    jg  problem
+
+    jmp no_problem
+
+problem:
+    ;; if it is 0 put nothing and substract index
+    mov byte[eax], ''
+    mov eax, [index]
+    dec eax
+    mov [index], eax
+    
+
+no_problem:
+
+    pop ebx 
+    
+    mov eax, [index]
+    inc eax
+    mov [index], eax
+
+    pop eax
     pop edi
-    inc si
 
     ;; see if we completed the column and push the number of lines back on the stack
     inc eax
@@ -68,6 +116,13 @@ column_loop:
     inc ecx
     cmp ecx, [len_cheie]
     jne columnar_loop
+
+
+debug:
+    mov eax, [index]
+    mov byte[ebx + eax], 0
+
+    pop edx
 
     ;; FREESTYLE ENDS HERE
     ;; DO NOT MODIFY
