0f2f918154941e8d9b7a41dfa9859554e3a20039
Mihailescu Eduard <mihailescu.eduard@gmail.com>
2021-12-02 15:14:48 +0200

    Finished task2

--- ages.asm
+++ ages.asm
@@ -1,3 +1,4 @@
+%include "io.mac.asm"
 ; This is your structure
 struc  my_date
     .day: resw 1
@@ -5,8 +6,11 @@ struc  my_date
     .year: resd 1
 endstruc
 
+
 section .text
     global ages
+
+extern printf
 
 ; void ages(int len, struct my_date* present, struct my_date* dates, int* all_ages);
 ages:
@@ -24,6 +28,44 @@ ages:
     ;; TODO: Implement ages
     ;; FREESTYLE STARTS HERE
 
+main_loop:
+    xor     eax, eax
+    xor     ebx, ebx
+    mov     ax, word[edi + my_date_size * (edx - 1) + my_date.year] ;; get the year
+    mov     bx, word[esi + my_date.year] ;; get the current year
+    sub     bx, ax
+    
+
+    mov     ax, word[edi + my_date_size * (edx - 1) + my_date.month] ;; get the month
+    cmp     ax, word[esi + my_date.month]
+    jl      continue
+
+    cmp     ax, word[esi + my_date.month]
+    je      same_month
+
+    dec     bx
+    jmp     continue
+
+same_month:
+    mov     ax, word[edi + my_date_size * (edx - 1) + my_date.day] ;; get the day
+    cmp     ax, word[esi + my_date.day]
+    jle      continue
+
+    dec     bx
+
+    cmp     bx, 0xffff
+    jne     continue
+
+    mov     bx, 0
+
+continue:
+
+    ; PRINTF32 `%u\n\x0`, ebx
+    mov     [ecx + 4 * (edx - 1)], ebx
+
+    dec     edx
+    cmp     edx, 0
+    jne     main_loop
 
     ;; FREESTYLE ENDS HERE
     ;; DO NOT MODIFY
--- columnar.asm
+++ columnar.asm
@@ -1,8 +1,11 @@
+%include "io.mac.asm"
 section .data
     extern len_cheie, len_haystack
 
 section .text
     global columnar_transposition
+
+extern printf
 
 ;; void columnar_transposition(int key[], char *haystack, char *ciphertext);
 columnar_transposition:
@@ -18,7 +21,53 @@ columnar_transposition:
 
     ;; TODO: Implment columnar_transposition
     ;; FREESTYLE STARTS HERE
-    
+
+    ; PRINTF32 `%u\n\x0`, ecx
+
+    ; Iterate through vector
+
+    ;; ecx iterate through key vector [0, len_cheie -1]
+    ;; eax iterate through the column at index ecx
+
+    xor edx, edx
+
+    ;; get the number of lines and store aon the stack
+    mov eax, [len_haystack]
+    mov ecx, [len_cheie]
+    div ecx
+    inc eax
+    push eax
+
+
+    xor ecx, ecx
+    xor si, si
+
+columnar_loop:
+
+    xor eax, eax
+    ;; index of the current column
+    mov edx, [edi + (ecx * 4)]
+
+column_loop:
+    push edi
+    mov edi, [len_cheie]
+    imul edi, eax
+    mov [ebx + si], [esi + edi]
+    pop edi
+    inc si
+
+    ;; see if we completed the column and push the number of lines back on the stack
+    inc eax
+    pop edx
+    push edx
+    cmp eax, edx
+    jne column_loop
+
+
+    ;; check if finish
+    inc ecx
+    cmp ecx, [len_cheie]
+    jne columnar_loop
 
     ;; FREESTYLE ENDS HERE
     ;; DO NOT MODIFY
