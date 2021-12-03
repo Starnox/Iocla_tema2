175e088b73db3c7f3216722f72b9d17a0a710659
Mihailescu Eduard <mihailescu.eduard@gmail.com>
2021-12-03 12:51:51 +0200

    Started work on task 4

--- cache.asm
+++ cache.asm
@@ -4,6 +4,12 @@ CACHE_LINE_SIZE EQU 8
 OFFSET_BITS  EQU 3
 TAG_BITS EQU 29 ; 32 - OFSSET_BITS
 
+section .data
+    reg_address dd 0
+    tags        dd 0
+    cache       dd 0
+    address     dd 0
+    to_replace  dd 0
 
 section .text
     global load
@@ -25,7 +31,13 @@ load:
 
     ;; TODO: Implment load
     ;; FREESTYLE STARTS HERE
-    
+
+    ;; put all the parameters in global variables so we have registers to work it
+    mov [reg_address], eax
+    mov [tags], ebx
+    mov [cache], ecx
+    mov [address], edx
+    mov [to_replace], edi     
 
     ;; FREESTYLE ENDS HERE
     ;; DO NOT MODIFY
--- columnar.asm
+++ columnar.asm
@@ -1,4 +1,3 @@
-%include "io.mac.asm"
 section .data
     extern len_cheie, len_haystack
     index dd 0
@@ -6,7 +5,6 @@ section .data
 section .text
     global columnar_transposition
 
-extern printf
 
 ;; void columnar_transposition(int key[], char *haystack, char *ciphertext);
 columnar_transposition:
@@ -23,19 +21,14 @@ columnar_transposition:
     ;; TODO: Implment columnar_transposition
     ;; FREESTYLE STARTS HERE
 
-    ; PRINTF32 `%u\n\x0`, ecx
-
-    ; Iterate through vector
-
-    ;; ecx iterate through key vector [0, len_cheie -1]
-    ;; eax iterate through the column at index ecx
+    
     mov eax, [index]
     xor eax, eax
     mov [index], eax
 
     xor edx, edx
 
-    ;; get the number of lines and store aon the stack
+    ;; get the number of lines and store on the stack
     mov eax, [len_haystack]
     mov ecx, [len_cheie]
     div ecx
@@ -43,15 +36,22 @@ columnar_transposition:
     push eax
 
 
+    ;; reset ecx register
     xor ecx, ecx
+
+    ; Iterate through vector
+
+    ;; ecx iterate through key vector [0, len_cheie -1]
+    ;; eax iterate through the column at index ecx
 
 columnar_loop:
 
     xor eax, eax
-    ;; index of the current column
+    
     
 
 column_loop:
+    ;; index of the current column
     mov edx, [edi + (ecx * 4)]
 
     ;; get the index of the char in plain text
@@ -59,22 +59,29 @@ column_loop:
     mov edi, [len_cheie]
     imul edi, eax
     
-    ;; verify 
 
     ;; save the current index stored in eax
     ;; and put the index of the current character of cypertext
     push eax
     mov eax, [index]
 
+    ;; the index of cypertext where the next character should be
     lea eax, [ebx + eax]
+
     push ebx
     mov ebx, esi
+
+    ;; get the character from haystack
     lea ebx, [ebx + edi]
     lea ebx, [ebx + edx] 
 
     mov bl, byte[ebx]
+
+    ;; add the character to cypertext
     mov byte[eax], bl
 
+
+    ;; verify if we have a valid byte
     cmp bl, 31
     jle problem
 
@@ -86,7 +93,8 @@ column_loop:
     jmp no_problem
 
 problem:
-    ;; if it is 0 put nothing and substract index
+    ;; not a valid byte in our string
+    ;; put nothing and decrement the index
     mov byte[eax], ''
     mov eax, [index]
     dec eax
@@ -117,11 +125,11 @@ no_problem:
     cmp ecx, [len_cheie]
     jne columnar_loop
 
-
-debug:
+    ;; put \0 to mark the end of the string
     mov eax, [index]
     mov byte[ebx + eax], 0
 
+    ;; clear the stack
     pop edx
 
     ;; FREESTYLE ENDS HERE
